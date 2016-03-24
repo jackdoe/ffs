db6793f4f37e36add609cf274f1bb5955d2682f6
borislav nikolov <jack@sofialondonmoskva.com>
2016-03-24 15:53:20 +0100

    move to finatra + sbt

--- README.txt
+++ README.txt
@@ -1,12 +1,12 @@
-ffs.sh - generate basic java project skeleton with rapidoid, junit and a fucking makfile
+ffs.sh - generate basic java project skeleton with finatra
 
 * creates {name}/src/main/java/{name}
 * creates {name}/src/main/resources
 * creates {name}/src/test/java/{name}
 * creates {name}/src/test/resources
-* creates {name}/pom.xml { adds dependency on rapidoid and junit }
-* creates {name}/src/main/java/{name}/Main.java { starts rapidoid }
-* creates {name}/Makefile { runs mvn package && java -cp with port 5005 for remote debug }
+* creates {name}/build.sbt { with finatra }
+* creates {name}/src/main/java/{name}/Main.java { starts finatra }
+* creates {name}/src/main/java/{name}/MainServer.java { sets up controller and basic response}
 
 -----
 
@@ -21,41 +21,35 @@ mkdir: created directory ‘example/src/test’
 mkdir: created directory ‘example/src/test/java’
 mkdir: created directory ‘example/src/test/java/example’
 mkdir: created directory ‘example/src/test/resources’
-creating example/pom.xml
+creating example/build.sbt
 creating example/src/main/java/example/Main.java
-creating example/Makefile
+creating example/src/main/java/example/MainServer.java
 jack@foo $
-jack@foo $ cd example && make
-mvn package && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp target/dependency/*:target/example-app-0.1-SNAPSHOT.jar example.Main port=8080 address=127.0.0.1 cpus=2 workers=4 nodelay
-...
-...
-...
-... bla bla bla ...
-Listening for transport dt_socket at address: 5005
-INFO | server | org.rapidoid.net.impl.AbstractLoop | Starting event loop | name=server
-INFO | server | org.rapidoid.net.impl.RapidoidServerLoop | Initializing server | port=8080 | accept=non-blocking
-INFO | server | org.rapidoid.net.impl.RapidoidServerLoop | Opening port to listen | port=8080
-INFO | server | org.rapidoid.net.impl.RapidoidServerLoop | Opened server socket | address=/0.0.0.0:8080
-INFO | server | org.rapidoid.net.impl.RapidoidServerLoop | Registering accept selector
-INFO | server | org.rapidoid.net.impl.RapidoidServerLoop | Waiting for connections...
-INFO | server1 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=buffers | creatorThread=server1 | class=SynchronizedArrayPool
-INFO | server1 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=connections | creatorThread=server1 | class=SynchronizedArrayPool
-INFO | server1 | org.rapidoid.net.impl.AbstractLoop | Starting event loop | name=server1
-INFO | server2 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=buffers | creatorThread=server2 | class=SynchronizedArrayPool
-INFO | server2 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=connections | creatorThread=server2 | class=SynchronizedArrayPool
-INFO | server2 | org.rapidoid.net.impl.AbstractLoop | Starting event loop | name=server2
-INFO | server3 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=buffers | creatorThread=server3 | class=SynchronizedArrayPool
-INFO | server3 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=connections | creatorThread=server3 | class=SynchronizedArrayPool
-INFO | server3 | org.rapidoid.net.impl.AbstractLoop | Starting event loop | name=server3
-INFO | server4 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=buffers | creatorThread=server4 | class=SynchronizedArrayPool
-INFO | server4 | org.rapidoid.insight.AbstractInsightful | Creating object | kind=pool | name=connections | creatorThread=server4 | class=SynchronizedArrayPool
-INFO | server4 | org.rapidoid.net.impl.AbstractLoop | Starting event loop | name=server4
-...
+jack@foo $ cd example
+jack@foo $ sbt run
+[info] Set current project to example (in build file:example/)
+[info] Updating {file:example/}example...
+[info] Resolving jline#jline;2.12.1 ...
+[info] Done updating.
+[info] Compiling 2 Java sources to example/target/classes...
+[info] Running example.Main 
+Mar 24, 2016 3:51:43 PM com.twitter.finagle.http.HttpMuxer$$anonfun$4 apply
+INFO: HttpMuxer[/admin/metrics.json] = com.twitter.finagle.stats.MetricsExporter(<function1>)
+Mar 24, 2016 3:51:43 PM com.twitter.finagle.http.HttpMuxer$$anonfun$4 apply
+INFO: HttpMuxer[/admin/per_host_metrics.json] = com.twitter.finagle.stats.HostMetricsExporter(<function1>)
+15:51:43.083 [run-main-0] INFO  example.MainServer - Process started
+I 0324 14:51:43.227 THREAD21:  => com.twitter.server.handler.AdminRedirectHandler
+I 0324 14:51:43.228 THREAD21: /admin => com.twitter.server.handler.SummaryHandler
+I 0324 14:51:43.229 THREAD21: /admin/server_info => com.twitter.finagle.Filter$$anon$1
+I 0324 14:51:43.233 THREAD21: /admin/lint => com.twitter.server.handler.LintHandler
+I 0324 14:51:43.233 THREAD21: /admin/lint.json => com.twitter.server.handler.LintHandler
+I 0324 14:51:43.233 THREAD21: /admin/threads => com.twitter.server.handler.ThreadsHandler
+I 0324 14:51:43.234 THREAD21: /admin/threads.json => com.twitter.server.handler.ThreadsH
 ----
 
 TODO:
 
-accept arguments with dependencies, and automatically write the pom.xml
+accept arguments with dependencies, and automatically write into build.sbt
 file based on something like:
 
 curl -s 'http://search.maven.org/solrsearch/select?q=rapidoid&rows=1&wt=json' | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["response"]["docs"][0]'
--- ffs.sh
+++ ffs.sh
@@ -9,73 +9,106 @@ name=$1
 if [ -z "$name" ]; then echo "usage: $0 projectname"; exit 1 ; fi
 
 mkdir -vp $name/src/main/java/$name $name/src/main/resources $name/src/test/java/$name $name/src/test/resources
-pexit $? && echo creating $name/pom.xml && cat > $name/pom.xml <<EOF
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-  <modelVersion>4.0.0</modelVersion>
-  <groupId>$name</groupId>
-  <artifactId>$name-app</artifactId>
-  <packaging>jar</packaging>
-  <description>$name</description>
-  <version>0.1-SNAPSHOT</version>
+pexit $? && echo creating $name/build.sbt && cat > $name/build.sbt <<EOF
+name := "$name"
+organization := "$name"
+version := "2.1.5"
+scalaVersion := "2.11.7"
+parallelExecution in ThisBuild := false
+publishMavenStyle := true
+crossPaths := false
+autoScalaLibrary := false
 
-  <dependencies>
-    <dependency>
-      <groupId>org.rapidoid</groupId>
-      <artifactId>rapidoid-http-fast</artifactId>
-      <version>5.0.12</version>
-    </dependency>
-    <dependency>
-      <groupId>junit</groupId>
-      <artifactId>junit</artifactId>
-      <version>4.12</version>
-      <scope>test</scope>
-    </dependency>
-  </dependencies>
+javacOptions ++= Seq(
+  "-source", "1.8",
+  "-target", "1.8",
+  "-Xlint:unchecked"
+)
 
-  <properties>
-    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-    <maven.compiler.source>1.8</maven.compiler.source>
-    <maven.compiler.target>1.8</maven.compiler.target>
-    <maven.compiler.testSource>1.8</maven.compiler.testSource>
-    <maven.compiler.testTarget>1.8</maven.compiler.testTarget>
-  </properties>
+mainClass in (Compile, packageBin) := Some("$name.Main")
 
-  <build>
-    <plugins>
-      <plugin>
-        <artifactId>maven-dependency-plugin</artifactId>
-        <executions>
-          <execution>
-            <phase>process-sources</phase>
-            <goals>
-              <goal>copy-dependencies</goal>
-            </goals>
-            <configuration>
-              <outputDirectory>\${targetdirectory}</outputDirectory>
-            </configuration>
-          </execution>
-        </executions>
-      </plugin>
-    </plugins>
-  </build>
-</project>
+lazy val versions = new {
+  val finatra = "2.1.5"
+  val guice = "4.0"
+  val logback = "1.0.13"
+}
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("releases"),
+  "Twitter Maven" at "https://maven.twttr.com"
+)
+
+libraryDependencies ++= Seq(
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra,
+  "com.twitter.finatra" %% "finatra-httpclient" % versions.finatra,
+  "ch.qos.logback" % "logback-classic" % versions.logback,
+
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra % "test",
+  "com.twitter.finatra" %% "finatra-jackson" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-server" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-app" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-core" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-modules" % versions.finatra % "test",
+  "com.google.inject.extensions" % "guice-testlib" % versions.guice % "test",
+
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra % "test" classifier "tests",
+  "com.twitter.finatra" %% "finatra-jackson" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-server" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-app" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-core" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-modules" % versions.finatra % "test" classifier "tests",
+
+  "org.mockito" % "mockito-core" % "1.9.5" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.3" % "test",
+  "org.specs2" %% "specs2" % "2.3.12" % "test",
+  "com.novocode" % "junit-interface" % "0.11" % Test)
 EOF
-
 pexit $? && echo creating $name/src/main/java/$name/Main.java && cat > $name/src/main/java/$name/Main.java <<EOF
 package $name;
-import org.rapidoid.data.*;
-import org.rapidoid.http.fast.*;
-import org.rapidoid.config.*;
 public class Main {
     public static void main(String[] args) {
-        Conf.args(args);
-        On.get("/size").json("msg", msg -> msg.length());
+        new MainServer().main(args);
     }
 }
 EOF
 
-pexit $? && echo creating $name/Makefile && cat > $name/Makefile <<EOF
-all:
-	 mvn package && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp target/dependency/*:target/$name-app-0.1-SNAPSHOT.jar $name.Main port=8080 address=127.0.0.1 cpus=2 workers=4 nodelay
+pexit $? && echo creating $name/src/main/java/$name/MainServer.java && cat > $name/src/main/java/$name/MainServer.java <<EOF
+package $name;
+import java.util.Collection;
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Module;
+import com.twitter.finatra.http.JavaHttpServer;
+import com.twitter.finatra.http.filters.CommonFilters;
+import com.twitter.finatra.http.routing.HttpRouter;
+import javax.inject.Inject;
+import com.twitter.finatra.http.JavaController;
+
+public class MainServer extends JavaHttpServer {
+    public static class MainController extends JavaController {
+        public class GoodbyeResponse {
+            public final String name;
+            public final String message;
+            public final Integer code;
+
+            public GoodbyeResponse(String name, String message, Integer code) {
+                this.name = name;
+                this.message = message;
+                this.code = code;
+            }
+        }
+
+        public void configureRoutes() {
+            get("/goodbye", request ->
+                    new GoodbyeResponse("guest", "cya", 123));
+        }
+    }
+
+
+    @Override
+    public void configureHttp(HttpRouter httpRouter) {
+        httpRouter
+            .filter(CommonFilters.class)
+            .add(MainController.class);
+    }
+}
 EOF
-pexit $?
